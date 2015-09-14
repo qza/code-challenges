fc1db8a2e1d6ec3205decc50d3a3d0eeb881bf78
qza <kokovic.zoran@gmail.com>
2015-09-14 20:36:18 +0200

    ZigZagSequence solution

--- src/main/java/codechallenges/dynamicprogramming/ZigZagSequence.java
+++ src/main/java/codechallenges/dynamicprogramming/ZigZagSequence.java
@@ -1,5 +1,6 @@
 package codechallenges.dynamicprogramming;
 
+import static java.lang.Math.max;
 import java.util.Arrays;
 
 /**
@@ -9,7 +10,7 @@ import java.util.Arrays;
  * successive numbers strictly alternate between positive and negative.
  *
  * @see http://community.topcoder.com/stat?c=problem_statement&pm=1259
- * 
+ *
  */
 public class ZigZagSequence {
 
@@ -17,25 +18,47 @@ public class ZigZagSequence {
 
         int[] difs = new int[sequence.length];
         int[] values = new int[sequence.length];
-        
+
         Arrays.fill(difs, 0);
-        
+
         values[0] = 1;
         values[1] = values[0] != values[1] ? 2 : 1;
-        
-        for(int i = 2; i < sequence.length; i++) {
-            int prediff = sequence[i - 1] - sequence[i-2];
-            int diff = sequence[i] - sequence[i];
-        } 
-        
-        return null;
+
+        for (int i = 2; i < sequence.length; i++) {
+            int prediff = sequence[i - 1] - sequence[i - 2];
+            int diff = sequence[i] - sequence[i - 1];
+            if ((prediff <= 0 && diff > 0) || (prediff >= 0 && diff < 0)) {
+                values[i] = values[i - 1] + 1;
+            } else {
+                values[i] = diff == 0 ? 1 : 2;
+            }
+        }
+
+        int maxIndex = 0, maxValue = 1;
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] > maxValue) {
+                maxIndex = i;
+                maxValue = values[i];
+            }
+        }
+
+        int[] result = new int[maxValue];
+        for (int counter = 0, i = maxIndex - maxValue + 1;
+                i < max(maxIndex + 1, maxValue); i++, counter++) {
+            result[counter] = sequence[i];
+        }
+
+        return result;
     }
-    
+
     public static void main(String[] args) {
         ZigZagSequence solver = new ZigZagSequence();
         int[] sequence = new int[]{0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0};
         int[] expected = new int[]{1, 0, 1, 0, 1, 0};
-        assert Arrays.equals(expected, solver.solve(sequence));
+        int[] result = solver.solve(sequence);
+        boolean asExpected = Arrays.equals(expected, result);
+        System.out.println(asExpected);
+        assert asExpected;
     }
 
 }
--- src/test/java/codechallenges/dynamicprogramming/ZigZagSequenceTest.java
+++ src/test/java/codechallenges/dynamicprogramming/ZigZagSequenceTest.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 import java.util.Collection;
 import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 /**
  * ZigZagSequence test class
  *
  */
+@RunWith(Parameterized.class)
 public class ZigZagSequenceTest {
 
     static final int MAX_WEIGHT = 100;
@@ -25,9 +27,9 @@ public class ZigZagSequenceTest {
     @Parameterized.Parameters
     public static Collection<int[][]> parameters() {
         int[][][] params = new int[][][]{
-            {{0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0}, {1, 0, 1, 0, 1, 0}},
             {{1, 10, 2, 20, 3, 30}, {1, 10, 2, 20, 3, 30}},
-            {{1, 6, 8, 2, 3, 5, 6, 5, 2, 0}, {6, 8, 2, 3}}
+            {{1, 6, 8, 2, 3, 5, 6, 5, 2, 0}, {6, 8, 2, 3}},
+            {{0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0}, {1, 0, 1, 0, 1, 0}}
         };
         return Arrays.asList(params);
     }

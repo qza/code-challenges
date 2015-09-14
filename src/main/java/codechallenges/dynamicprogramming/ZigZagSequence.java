b3d2d18b83805df8c1a696d5d0eda632d94c5b5a
qza <kokovic.zoran@gmail.com>
2015-09-14 19:33:30 +0200

    ZigZagSequence initial commit

---
+++ src/main/java/codechallenges/dynamicprogramming/ZigZagSequence.java
@@ -0,0 +1,17 @@
+package codechallenges.dynamicprogramming;
+
+/**
+ * ZigZagSequence
+ *
+ * A sequence of numbers is called a zig-zag sequence when differences between
+ * successive numbers strictly alternate between positive and negative.
+ *
+ * @see http://community.topcoder.com/stat?c=problem_statement&pm=1259
+ */
+class ZigZagSequence {
+
+    public int[] solve(int[] sequence) {
+        return null;
+    }
+
+}
---
+++ src/test/java/codechallenges/dynamicprogramming/ZigZagSequenceTest.java
@@ -0,0 +1,41 @@
+package codechallenges.dynamicprogramming;
+
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+/**
+ * ZigZagSequence test class
+ *
+ */
+public class ZigZagSequenceTest {
+
+    static final int MAX_WEIGHT = 100;
+
+    int[] sequence;
+    int[] expected;
+
+    public ZigZagSequenceTest(int[] sequence, int[] expected) {
+        this.sequence = sequence;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<int[][]> parameters() {
+        int[][][] params = new int[][][]{
+            {{0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0}, {1, 0, 1, 0, 1, 0}},
+            {{1, 10, 2, 20, 3, 30}, {1, 10, 2, 20, 3, 30}},
+            {{1, 6, 8, 2, 3, 5, 6, 5, 2, 0}, {6, 8, 2, 3}}
+        };
+        return Arrays.asList(params);
+    }
+
+    @Test
+    public void shouldFindExpectedLongestSequences() {
+        ZigZagSequence solver = new ZigZagSequence();
+        assertArrayEquals(expected, solver.solve(sequence));
+    }
+
+}

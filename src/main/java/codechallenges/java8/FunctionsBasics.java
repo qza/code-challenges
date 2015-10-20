5e7a8488d8c4c84824149895b86277d7b09422ae
qza <kokovic.zoran@gmail.com>
2015-10-20 19:26:16 +0200

    sayCallable example in java8 functions

--- src/main/java/codechallenges/java8/FunctionsBasics.java
+++ src/main/java/codechallenges/java8/FunctionsBasics.java
@@ -3,6 +3,10 @@ package codechallenges.java8;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import java.util.function.IntBinaryOperator;
 import java.util.function.Predicate;
 
@@ -25,7 +29,13 @@ public class FunctionsBasics {
 		return Arrays.stream(data).parallel().reduce(0, intBinaryOperator);
 	}
 
-	public static void main(String[] args) {
+	static Callable<String> sayCallable(String sayWhat) {
+		return () -> {
+			return sayWhat;
+		};
+	}
+
+	public static void main(String[] args) throws Exception {
 
 		List<String> stringList = Arrays.asList("", "a", "", "b", "c", "", null, " ", "d");
 		System.out.println("# = " + FunctionsBasics.filterNonEmpty(stringList).size());
@@ -36,6 +46,8 @@ public class FunctionsBasics {
 		int[] maxData = new int[] { 1, 7, 4, 6, 0, 8, 3, 9 };
 		System.out.println("max = " + FunctionsBasics.max(maxData));
 
+		Future<String> callableResponse = Executors.newSingleThreadExecutor().submit(FunctionsBasics.sayCallable("hi"));
+		System.out.println("callable: " + callableResponse.get());
 	}
 
 }
